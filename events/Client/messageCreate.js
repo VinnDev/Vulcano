7b3f629409ca6204b5c2d4342f78ae98b643f4ef
vinnofficial@outlook.com <vinnofficial@outlook.com>
2023-02-13 23:23:10 +0700

    fix

--- events/Client/messageCreate.js
+++ events/Client/messageCreate.js
@@ -10,15 +10,14 @@ export default (client, message) => {
     if (!command) return;
 
     if (command.usage && !Context.args.length) {
-        const example = `❌ Please input the command arguments! Example: \`${client.prefix}${command.name} [${command.usage.split("|").map(usage => `\`${usage}\``).join(" / ")}]\``;
-        
+        return message.reply(`❌ Please input the command arguments! Example: \`${client.prefix}${command.name} [${command.usage.split("|").map(usage => `\`${usage}\``).join(" / ")}]\``);
     }
 
     Context.player = client.vulkava.players.get(message.guildId);
     if (command.optional.isPlaying && !Context.player.playing) return message.reply("There is no track playing.");
     if (command.optional.inVoiceChannel && !message.member.voice?.channel) return message.reply("You are not in a voice channel.");
     if (command.optional.inActive && message.guild.members.me.voice?.channel && Context.player.playing) {
-        if (message.guild.members.me.voice?.channel.members.size > 1 && Context.player.requester.id !== message.author.id) return message.reply("I have been active in the voice channel.");
+        if (message.guild.members.me.voice?.channel.members.size > 1 && Context.player.current.requester.id !== message.author.id) return message.reply("I have been active in the voice channel.");
     }
     if (command.optional.sameVoiceChannel && message.member.voice.channelId !== message.guild.members.me.voice.channelId) return message.reply("You are not in a same voice channel with me.");
 
