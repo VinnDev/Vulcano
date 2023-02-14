55d52f5bb84ff5ebb1be06f0a35384f7f9bbccb6
vinnofficial@outlook.com <vinnofficial@outlook.com>
2023-02-15 00:15:07 +0700

    fix a bit

--- commands/Music/leave.js
+++ commands/Music/leave.js
@@ -8,12 +8,7 @@ export default {
     execute: (client, message, ctx) => {
         if (!message.guild.members.me.voice?.channel) return message.reply({ embeds: [ctx.embed().setDescription(`Currently not active in voice channel.`)] });
 
-        if (ctx.player.playing) {
-            ctx.player.destroy();
-        }
-        else {
-            ctx.player.disconnect();
-        }
+        ctx.player.destroy();
 
         message.reply({ embeds: [ctx.embed().setDescription(`Leaving the voice channel 🔊 \`${message.guild.members.me.voice.channel.name}\``)] });
     }
--- events/Vulkava/queueEnd.js
+++ events/Vulkava/queueEnd.js
@@ -1,4 +1,6 @@
 export default (client, player) => {
+    player.destroy();
+
     const channel = client.channels.cache.get(player.textChannelId);
 
     channel.send({ embeds: [client.embed({ description: "Queue ended!" })] });
