0372f65f11987b15efc5faf557cee711b3a96e0e
vinnofficial@outlook.com <vinnofficial@outlook.com>
2023-02-15 04:06:41 +0700

    update version 1.1.0-b (beta)

--- events/Vulkava/trackStart.js
+++ events/Vulkava/trackStart.js
@@ -1,5 +1,8 @@
 export default (client, player, track) => {
-    if (player.message) player.message.delete().catch(o_O => void 0);
+    if (player.message) {
+        player.message.delete().catch(o_O => void 0);
+        player.message = null;
+    };
 
     const channel = client.channels.cache.get(player.textChannelId);
 
