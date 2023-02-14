d224e17a2f4e156baa3ce5bbec6a95147b0b5d38
vinnofficial@outlook.com <vinnofficial@outlook.com>
2023-02-14 16:42:06 +0700

    update embed message colors

--- events/Client/messageCreate.js
+++ events/Client/messageCreate.js
@@ -43,7 +43,7 @@ export default (client, message) => {
     if (command.optional.sameVoiceChannel && message.guild.members.me.voice?.channel && message.member.voice.channelId !== message.guild.members.me.voice.channelId) return message.reply({ embeds: [embed.setDescription("You are not in a same voice channel with me.")] });
 
     try {
-        Context.embed = (options = { color: client.config.colors }) => {
+        Context.embed = (options = { color: client.config.colors.replace("#", "0x") }) => {
             if (!options.color) options.color = client.config.colors.replace("#", "0x");
             return new EmbedBuilder(options);
         }
