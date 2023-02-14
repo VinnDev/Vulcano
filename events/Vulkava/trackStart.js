f0af1249c965608d1b5425dce51453ca9e93e77b
vinnofficial@outlook.com <vinnofficial@outlook.com>
2023-02-14 20:16:32 +0700

    async / await

--- events/Vulkava/trackStart.js
+++ events/Vulkava/trackStart.js
@@ -1,7 +1,7 @@
-export default (client, player, track) => {
-    client.setupTrackStart(player);
+export default async (client, player, track) => {
+    await client.setupTrackStart(player);
 
     const channel = client.channels.cache.get(player.textChannelId);
 
-    player.message = channel.send({ embeds: [client.embed({ description: `Start playing [${track.title}](${track.uri})` })] }).then(message => player.setupMessage(message));
+    player.message = await channel.send({ embeds: [client.embed({ description: `Start playing [${track.title}](${track.uri})` })] }).then(message => player.setupMessage(message));
 }
