7f468133e201f9af2b8590e8a3cfbf10da09ba8e
vinnofficial@outlook.com <vinnofficial@outlook.com>
2023-02-20 02:17:57 +0700

    update config lavalink

--- settings/nodes.js
+++ settings/nodes.js
@@ -1,7 +1,7 @@
 export default [{
     id: "Primary",
     hostname: process.env.LAVA_HOSTNAME ?? "localhost",
-    port: process.env.LAVA_PORT ?? 2333,
+    port: Number(process.env.LAVA_PORT) ? Number(process.env.LAVA_PORT) : 2333,
     password: process.env.LAVA_PASSWORD ?? "youshallnotpass",
     secure: false
 }]
