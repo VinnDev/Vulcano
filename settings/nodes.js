700f25b06fa2dce88efc146e9ea9beca3989de80
vinnofficial@outlook.com <vinnofficial@outlook.com>
2023-02-20 02:13:26 +0700

    lavalink nodes

--- settings/nodes.js
+++ settings/nodes.js
@@ -1,14 +1,7 @@
 export default [{
     id: "Primary",
     hostname: process.env.LAVA_HOSTNAME ?? "localhost",
-    port: validateNumber(process.env.LAVA_PORT) || 2333,
+    port: process.env.LAVA_PORT ?? 2333,
     password: process.env.LAVA_PASSWORD ?? "youshallnotpass",
     secure: false
 }]
-
-function validateNumber(value) {
-    value = parseInt(value);
-
-    if (!value) throw "Error at here! You must check and setup enviroment value must Number";
-    return value;
-}
